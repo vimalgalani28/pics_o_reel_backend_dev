507da381fc6599d88904fc5e174f4349c524a4b9
codarc264 <vimalgalani28@gmail.com>
2021-05-16 20:13:35 +0530

    Leaderboard Route

--- src/routes/entryPublic.js
+++ src/routes/entryPublic.js
@@ -23,17 +23,20 @@ router.get("/allentries", auth, async (req, res) => {
   }
 });
 
-router.get("/count", authcoord, async (req, res) => {
+router.get("/leaderboard", authcoord, async (req, res) => {
   try {
-    if (!req.query.section) {
-      const entries = await Entry.count();
-      const users = await User.count();
-      const name = req.user.displayName;
-      return res.send({ entries, users, name });
-    }
-    const section = req.query.section;
-    const entries = await Entry.find({ section });
-    res.send(entries);
+    const entries = await Entry.find({})
+    const paintingCount = entries.filter(({ section }) => section === 'Painting').length
+    const photographyCount = entries.filter(({ section }) => section === 'Photography').length
+    const othersCount = entries.filter(({ section }) => section === 'Calligraphy').length
+    const independenceCount = entries.filter(({ section }) => section === 'Independence').length
+    return res.send({
+      entries,
+      paintingCount,
+      photographyCount,
+      othersCount,
+      independenceCount
+    });
   } catch (e) {
     res.send({
       status: 500,
