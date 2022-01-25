5cd4203d2bab38a6069b087aa40e4a844b221ed5
vimalgalani28 <vimalgalani28@gmail.com>
2022-01-25 10:10:06 +0530

    Final Live

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
